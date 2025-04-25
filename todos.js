77110310f837b6dc9ce2a2224aa68ad0c6a7a991
LebahLapar <akbarriffani19@gmail.com>
2025-04-25 17:38:50 +0700

    first commit

---
+++ todos.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+
+const todos = [{ ido: 'satu', titlo: 'Belajar Express', completed: false },
+    { ido: 'dua', titlo: 'Makan', completed: true }];
+
+app.get('/', (req,res) => {
+    res.send('Halo');
+});
+
+app.post('/todos', (req,res) => {
+    const todoBaru = req.body;
+    todos.push(todoBaru);
+    res.status(201).json({'benar ' : 'data berhasil ditambahkan!'
+    });
+
+})
+
+app.get('/todos', (req,res) => {
+    res.json(todos)
+})
+
+app.get('/todos/:id', (req,res) => {
+    const id = req.params.id;
+    const todo = todos.find(t => t.titlo === id);
+
+    if (!todo) {
+        return res.status(404).json({messege : "tidak ditemukan data"});
+    }
+
+    res.json(todo);
+})
+
+app.delete('/todos/hapus/:id', (req,res) => {
+    const hapus = req.params.id;
+    const FixHapus = todos.findIndex(t => t.title === hapus);
+
+    if (FixHapus == -1) {
+        return res.status(404).json({messege : "takde data"});
+
+    }
+
+    const HapusArr = todos.splice(FixHapus, 1);
+    res.json({
+        'status' : 'data berhasil dihapus!!',
+        'data' : HapusArr[0]
+    }
+    );
+});
+
+app.listen(3000, () => {
+    console.log("telah jalan")
+});
